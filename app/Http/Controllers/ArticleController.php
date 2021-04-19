4ac01dd0a31908919b418a03ee45059a865d2ef7
= <=>
2021-04-19 21:55:08 +0700

    Make a new view resource for interface view data and Modify ArticleController for data print

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use Barryvdh\DomPDF\Facade as PDF;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -108,4 +109,12 @@ class ArticleController extends Controller
     {
         //
     }
+
+    public static function cetak_pdf()
+    {
+        $articles = Article::all();
+        $pdf = PDF::loadView('articles.articles_pdf',['articles' => $articles]);
+        return $pdf->stream();
+    }
+
 }
